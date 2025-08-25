9fe75d5d3f223f4ec19fb475e87be9182457e7e3
张鹏 <416838606@qq.com>
2025-08-26 00:04:54 +0800

    update

--- exercises/07_loop/main.cpp
+++ exercises/07_loop/main.cpp
@@ -8,7 +8,7 @@ static unsigned long long fibonacci(int i) {
     // TODO: 为缓存设置正确的初始值
     static unsigned long long cache[96] = {0, 1}, cached = 2;
     // TODO: 设置正确的循环条件
-    for (; cached < i; ++cached) {
+    for (; cached <= i; ++cached) {
         cache[cached] = cache[cached - 1] + cache[cached - 2];
     }
     return cache[i];
